f6eedd6398629effab7d8687c191e61ed5e9325a
Akos Belakovics <belakovics.akos@gmail.com>
2013-05-16 02:09:08 +0200

    Mod: Server gui New: integration folder for integration purposes

---
+++ code/integrated/HFS/MainWindow.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HFS
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //ListDirectory(tvLocal, "C:\\");
+        }
+
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+
+            var directoryNodes = CreateDirectoryNode(rootDirectoryInfo);
+
+            if (directoryNodes != null)
+                treeView.Nodes.Add(directoryNodes);
+        }
+
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                try
+                {
+                    directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+                }
+                catch (Exception)
+                {
+                }
+            }
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                try
+                {
+                    directoryNode.Nodes.Add(new TreeNode(file.Name));
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            return directoryNode;
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(tbTag.Text))
+            {
+                epTag.SetError(tbTag, "Tag nem lehet üres!");
+                return;
+            }
+
+            epTag.Clear();
+
+            TagListItem tli = new TagListItem();
+
+            lRemote.Controls.Add(tli);
+            tli.TagText = tbTag.Text;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ code/integrated/HFS/TagListItem.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace HFS
+{
+    partial class TagListItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list = new System.Windows.Forms.ListBox();
+            this.label = new System.Windows.Forms.Label();
+            this.btRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // list
+            // 
+            this.list.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.list.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.list.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 15;
+            this.list.Location = new System.Drawing.Point(38, 28);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(229, 122);
+            this.list.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label.Location = new System.Drawing.Point(7, 6);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(260, 19);
+            this.label.TabIndex = 1;
+            this.label.Text = "Label";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btRemove
+            // 
+            this.btRemove.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.btRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btRemove.Location = new System.Drawing.Point(7, 27);
+            this.btRemove.Name = "btRemove";
+            this.btRemove.Size = new System.Drawing.Size(26, 124);
+            this.btRemove.TabIndex = 2;
+            this.btRemove.Text = "Remove";
+            this.btRemove.UseVisualStyleBackColor = true;
+            // 
+            // TagListItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.btRemove);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.list);
+            this.Name = "TagListItem";
+            this.Size = new System.Drawing.Size(273, 157);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btRemove;
+    }
+}
